fe7145366d7d0d0013b30fe615e81a6a29bb94d1
palaknayak006 <palak.nayak_cs23@gla.ac.in>
2025-08-23 23:53:29 +0530

    Add files via upload

---
+++ Controlflowslevel1/src/Ques14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ques14 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a positive integer: ");
+            int n = sc.nextInt();
+
+            if (n >= 0) {
+                long fact = 1;
+                int i = 1;
+                while (i <= n) {
+                    fact *= i;
+                    i++;
+                }
+                System.out.println("Factorial of " + n + " is " + fact);
+            } else {
+                System.out.println("Invalid input! Please enter a natural number.");
+            }
+        }
+    }
+}
+
---
+++ Controlflowslevel1/src/Ques18.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ques18 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
+
+            for (int i = 6; i <= 9; i++) {
+                System.out.println(n + " * " + i + " = " + (n * i));
+            }
+        }
+    }
+}
+
