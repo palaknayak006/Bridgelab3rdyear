cab873efd9765d67c747aa98cd8a7c1dfe92c811
palaknayak006 <palak.nayak_cs23@gla.ac.in>
2025-09-14 23:46:36 +0530

    Add files via upload

---
+++ this, static, final key words and instanceof operator/Level1/src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+   private static String libraryName;
+   private final String title;
+   private final String author;
+   private final String isbn;
+
+   public Book(String title, String author, String isbn) {
+       this.title = title;
+       this.author = author;
+       this.isbn = isbn;
+   }
+
+   public static void setLibraryName(String name) {
+       libraryName = name;
+   }
+
+   public static void displayLibraryName() {
+       System.out.println("Library Name: " + libraryName);
+   }
+
+   public void displayBookDetails() {
+       System.out.println("Title: " + title);
+       System.out.println("Author: " + author);
+       System.out.println("ISBN: " + isbn);
+   }
+
+   public String getTitle() { return title; }
+   public String getAuthor() { return author; }
+   public String getIsbn() { return isbn; }
+}
---
+++ this, static, final key words and instanceof operator/Level1/src/Library.java
@@ -0,0 +1,8 @@
+public class Library {
+   public static void main(String[] args) {
+       Book.setLibraryName("Egmore Library");
+       Book.displayLibraryName();
+       Book book1 = new Book("Effective Java", "Joshua Bloch", "978-0134685991");
+       book1.displayBookDetails();
+   }
+}
